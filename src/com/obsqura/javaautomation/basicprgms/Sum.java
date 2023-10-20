65842269f862352aee93d558135eb222e089797d
Reema <reemaalessa1998@gmail.com>
2023-10-20 07:30:37 +0530

    My first commit message

---
+++ src/com/obsqura/javaautomation/basicprgms/Sum.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.obsqura.javaautomation.basicprgms;
+
+/**
+ * 
+ */
+public class Sum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[]args) {
+	
+		float a=10.3f;
+		float b=10.2f;
+		float sum =a+b;
+
+		System.out.println("Sum of a and b is " +sum);
+		}
+		}
+	
---
+++ src/com/obsqura/javaautomation/basicprgms/areaOfTriangle.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.obsqura.javaautomation.basicprgms;
+
+/**
+ * 
+ */
+public class areaOfTriangle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+			double base= 7.4;
+			double height =7.3;
+			double area= 0.5*base*height;
+			System.out.println("Area of the triangle: " +area);
+			}
+	}
+
